87e2939d45ee71b9df75c17c1584e8505e16f21d
aliszson <aligbr150@gmail.com>
2024-07-13 15:25:55 -0300

    Adicionando variáveis e exemplos de tipos no arquivo TiposVariaveis.java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        double salarioMinino = 2500.13;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // casting
+
+        int numero = 5;
+        numero = 10;
+
+        final float VALOR_DE_PI = 3.14f; //final + CAIXA ALTA para n alterar o valor
+        System.out.println(numero);
+
+
+
     }
 }
