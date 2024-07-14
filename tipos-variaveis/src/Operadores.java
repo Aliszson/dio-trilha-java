ff33c0787a7133a646ceba66a823046f9286b9b9
aliszson <aligbr150@gmail.com>
2024-07-14 15:50:41 -0300

    operador ternário

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // operador tenrário
+        int a, b;
+        a = 8;
+        b = 8;
+        String resultado = a==b ?"verdadeira" : "falso";
+        /*     
+        if (a==b) {
+            resultado = "verdadeiro";
+        }else{
+            resultado = "falso";
+        }
+        
+         */
+        System.out.println(resultado);
+
+    }
+}
