95e151bd417c5a7d4d01fa61559e4f1c09a47e74
aliszson <aligbr150@gmail.com>
2024-07-17 15:33:59 -0300

    atualizando

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,11 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 5;
 
-        if (nota >= 7)
-            System.out.println("Aprovado");
-        else
-            System.out.println("Reprovado");   
+        //String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <=7 ? "Recuperação" : "Reprovado"; // else-if ternário
+        
+        System.out.println(resultado);
     }
 }
